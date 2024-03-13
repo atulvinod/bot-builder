28ce815de1015985c5f66769ef2eebc9f8e2ba7c
atulvinod <atulvinod1911@gmail.com>
2024-03-13 14:42:05 +0530

    fix apos

--- web-app/src/app/dashboard/bots/page.tsx
+++ web-app/src/app/dashboard/bots/page.tsx
@@ -46,7 +46,7 @@ export default async function ViewBotsPage() {
                             alt="no data"
                         />
                         <span className="text-gray-800 text-2xl mt-2">
-                            You don't have any bots yet
+                            You don&apos;t have any bots yet
                         </span>
                     </div>
                 </div>
