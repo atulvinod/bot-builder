ab3a1eb79b8dc18bbdef124fabd8ca3af426b0d7
atulvinod <39339155+atulvinod@users.noreply.github.com>
2024-03-14 19:47:53 +0530

    Update redis_client.py

--- chat-service/lib/clients/redis_client.py
+++ chat-service/lib/clients/redis_client.py
@@ -3,7 +3,7 @@ import os
 
 class RedisClient:
     def __init__(self) -> None:
-        self.__redisClient = Redis(url=os.getenv("REDIS_URL"))
+        self.__redisClient = Redis.from_url(os.getenv("REDIS_URL"))
         
     def getClient(self):
         return self.__redisClient
